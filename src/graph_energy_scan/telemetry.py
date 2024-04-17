49d9c0470a6c4663446e7faeaaf34eef356ce539
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2024-04-17 11:30:41 +0000

    Add appropriate metadata to traces

--- src/graph_energy_scan/telemetry.py
+++ src/graph_energy_scan/telemetry.py
@@ -1,11 +1,22 @@
 from opentelemetry import trace
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
+from opentelemetry.semconv.resource import ResourceAttributes
+
+from graph_energy_scan import __version__
 
 
 def setup_telemetry(otel_collector_url: str):
-    tracer_provider = TracerProvider()
+    tracer_provider = TracerProvider(
+        resource=Resource.create(
+            {
+                ResourceAttributes.SERVICE_NAME: "graph-energy-scan",
+                ResourceAttributes.SERVICE_VERSION: __version__,
+            }
+        )
+    )
     tracer_provider.add_span_processor(BatchSpanProcessor(ConsoleSpanExporter()))
     tracer_provider.add_span_processor(
         BatchSpanProcessor(OTLPSpanExporter(otel_collector_url))
