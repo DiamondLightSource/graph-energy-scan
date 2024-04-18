fb7247739235fee68b212d712dd88b6c36f65ecd
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2024-04-18 14:19:16 +0000

    Use aiomyql driver to generate models

--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
+import asyncio
 import os
 
 import setuptools
 from sqlacodegen_v2.generators import DeclarativeGenerator
-from sqlalchemy import MetaData, create_engine
+from sqlalchemy import MetaData
+from sqlalchemy.ext.asyncio import create_async_engine
 
-if __name__ == "__main__":
-    engine = create_engine(os.environ["DATABASE_URL"])
+
+async def generate_models():
+    engine = create_async_engine(os.environ["DATABASE_URL"])
     metadata = MetaData()
-    metadata.reflect(engine, only=["EnergyScan"])
-    generator = DeclarativeGenerator(metadata, engine, set())
+    async with engine.begin() as connection:
+        await connection.run_sync(
+            lambda connection: metadata.reflect(connection, only=["EnergyScan"])
+        )
+        generator = await connection.run_sync(
+            lambda connection: DeclarativeGenerator(metadata, connection, set())
+        )
     with open("src/graph_energy_scan/models.py", "w") as models_file:
         models_file.write(generator.generate())
+
+
+if __name__ == "__main__":
+    asyncio.run(generate_models())
     setuptools.setup()
